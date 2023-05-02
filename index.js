ce1365e1566d9e1b1eb49ea6f61cab603d3e1c2f
vaibhav <vaibhavsahni2001@gmail.com>
2023-05-02 11:24:44 +0530

    final2

--- index.js
+++ index.js
@@ -49,12 +49,11 @@ app.post("/voter/add", async (req, res) => {
   res.status(200).json(voter);
 });
 
-app.get("/voter/get/:name/:building/:id/:so/:booth", async (req, res) => {
+app.get("/voter/get/:name/:building/:so/:booth", async (req, res) => {
   let result = await Voter.find({
     $or: [
       { name: { $regex: req.params.name, $options: "i" } },
       { building: { $regex: req.params.building, $options: "i" } },
-      { userId: { $regex: req.params.id, $options: "i" } },
       { fatherName: { $regex: req.params.so, $options: "i" } },
       { boothNumber: { $regex: req.params.booth, $options: "i" } },
     ],
